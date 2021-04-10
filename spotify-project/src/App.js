3d20197f66cee7b6f837bc95cd4cb19bd1dff631
ShadmanAzad <73686426+ShadmanAzad@users.noreply.github.com>
2021-04-10 15:27:12 -0400

    Update

--- spotify-project/src/App.js
+++ spotify-project/src/App.js
@@ -1,16 +1,54 @@
 // import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
 
 function App() {
   return (
+
+    <>
+    <div className = "Nav">
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="/">Spotify Project</NavbarBrand>
+      <NavbarToggler  />
+      <Collapse navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="/components/">About</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/reactstrap/reactstrap">Sign Up</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/reactstrap/reactstrap">Login</NavLink>
+          </NavItem>
+        </Nav>
+    
+      </Collapse>
+    </Navbar>
+  </div>
     <div className="App">
+  
       <header className="">
         <p>
          <Welcome />
         </p>
       </header>
     </div>
+    </>
   );
 }
 
--- spotify-project/src/components/Welcome.js
+++ spotify-project/src/components/Welcome.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Description from './Description.js';
 
 
 class Welcome extends React.Component {
     render (){
-        return <h1>Welcome to Spotify Project</h1>;
+        return (
+        <div className = "Heading">
+            <h1>Welcome to Spotify Project</h1>
+                <Description />
+        </div>
+        )
     }
 }
 
 export default Welcome;
+
